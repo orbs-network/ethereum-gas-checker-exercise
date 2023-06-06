08b8eb51302b9fbc7841d804b640fc2bb6641052
Sebster <sebster.dev@gmail.com>
2023-06-06 05:12:06 +0100

    feat(GasTracker): consume getPriceData

--- src/components/GasTracker/GasTracker.tsx
+++ src/components/GasTracker/GasTracker.tsx
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
 
 import { getGasData } from "@src/services/etherscan/gastracker";
+import { getPriceData } from "@src/services/etherscan/ethprice";
 
-type Data = Awaited<ReturnType<typeof getGasData>>;
+type GasData = Awaited<ReturnType<typeof getGasData>>;
+type PriceData = Awaited<ReturnType<typeof getPriceData>>;
 
 export const GasTracker: React.FC = () => {
-	const [data, setData] = useState<Data>();
+	const [gasData, setGasData] = useState<GasData>();
+	const [priceData, setPriceData] = useState<PriceData>();
 
 	useEffect(() => {
-		getGasData().then((newData) => newData && setData(newData));
+		getGasData().then((newData) => newData && setGasData(newData));
+		getPriceData().then((newData) => newData && setPriceData(newData));
 	}, []);
+
+	const data = { gasData, priceData };
 
 	return <pre>{data && JSON.stringify(data, null, 2)}</pre>;
 };
