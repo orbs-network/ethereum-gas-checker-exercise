5d08aa9c923672954f5041b2865bb313ca9e10b6
Sebster <sebster.dev@gmail.com>
2023-06-06 05:12:07 +0100

    feat(styles): reset css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./components/App";
 
+import "./styles/reset.scss";
+
 const rootElement = document.getElementById("app");
 const app = <App />;
 
