5ca29c4faec6fbc797fc38f09076e210106f7770
Sebster <sebster.dev@gmail.com>
2023-06-06 05:12:07 +0100

    refactor(css): extract variables

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./components/App";
 
 import "./styles/reset.scss";
+import "./styles/theme.scss";
 
 const rootElement = document.getElementById("app");
 const app = <App />;
